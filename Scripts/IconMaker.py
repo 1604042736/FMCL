ec014666cb14fab50a6f6daa571570b6122fa041
YongjianWang <wangyongjian_501@hotmail.com>
2022-08-01 15:56:47 +0800

    Change icon

--- Scripts/IconMaker.py
+++ Scripts/IconMaker.py
@@ -10,20 +10,24 @@ def main():
         pass
     img = Image.new("RGB", (16, 16))
     # 画背景
+    b = 0
     for i in range(16):
         for j in range(16):
             r = 255-i*16
             g = 255-j*16
-            b = 255
             img.putpixel((i, j), (r, g, b))
+            b += 1
     # 画 "F"
     color = (0, 0, 0)
     for i in range(4, 12):
         img.putpixel((i, 2), color)
+        img.putpixel((i, 3), color)
     for i in range(3, 14):
         img.putpixel((4, i), color)
+        img.putpixel((5, i), color)
     for i in range(5, 12):
         img.putpixel((i, 7), color)
+        img.putpixel((i, 8), color)
     img.save(path)
 
 
