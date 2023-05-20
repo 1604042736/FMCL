4b8422fdfe41613032c7d24409cb657a22882f7a
YongjianWang <wangyongjian_501@hotmail.com>
2023-05-20 18:19:51 +0800

    解决pyzw文件无法正常打开的问题

--- Scripts/Pack.py
+++ Scripts/Pack.py
@@ -5,6 +5,8 @@ import shutil
 def main():
     if not os.path.exists("../Pack"):
         os.makedirs("../Pack")
+    with open("../Pack/__init__.py", mode="w", encoding="utf-8")as file:
+        file.write("")
     for i in (("Functions", "../FMCL/Functions"),
               ("Translations", "../FMCL/Translations")):
         shutil.make_archive(f"../Pack/{i[0]}Pack", "zip", i[1])
