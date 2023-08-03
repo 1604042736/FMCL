15026b8728a1a299e459425c8c430898a29835f3
YongjianWang <wangyongjian_501@hotmail.com>
2023-08-03 13:05:44 +0800

    解决游戏日志无法显示的问题

--- FMCL/Functions/Launcher/Launcher.py
+++ FMCL/Functions/Launcher/Launcher.py
@@ -4,7 +4,7 @@ import multitasking
 import qtawesome as qta
 from Core import Game
 from Kernel import Kernel
-from PyQt5.QtCore import QProcess, pyqtSlot
+from PyQt5.QtCore import QProcess, pyqtSignal, pyqtSlot
 from PyQt5.QtWidgets import QWidget
 
 from .ui_Launcher import Ui_Launcher
@@ -13,6 +13,8 @@ _translate = Kernel.translate
 
 
 class Launcher(QWidget, Ui_Launcher):
+    __commandGot = pyqtSignal(str, list)
+
     def __init__(self, game_name: str):
         super().__init__()
         self.setupUi(self)
@@ -20,16 +22,19 @@ class Launcher(QWidget, Ui_Launcher):
         self.setWindowIcon(qta.icon("mdi.rocket-launch-outline"))
         self.name = game_name
         self.te_output.setReadOnly(True)
-        
+        self.__commandGot.connect(self.__start)
 
     @multitasking.task
     def start(self):
         game = Game(self.name)
+        dir, command = game.get_launch_command()
+        logging.info(command)
+        self.__commandGot.emit(dir, command)
+
+    def __start(self, dir, command):
         self.process = QProcess()
         self.process.readyReadStandardOutput.connect(self.outputStandard)
         self.process.readyReadStandardError.connect(self.errorStandard)
-        dir, command = game.get_launch_command()
-        logging.info(command)
         program, *args = command
         self.process.setWorkingDirectory(dir)
         self.process.start(program, args)
