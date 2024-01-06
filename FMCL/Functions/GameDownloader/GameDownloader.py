dfea86f1167f717483e3d06b16a91828665b46f9
YongjianWang <wangyongjian_501@hotmail.com>
2024-01-06 19:09:30 +0800

    GameDownloader自动设置名称

--- FMCL/Functions/GameDownloader/GameDownloader.py
+++ FMCL/Functions/GameDownloader/GameDownloader.py
@@ -1,51 +1,95 @@
+import os
 import multitasking
 import qtawesome as qta
+
 from Core import Version
-from PyQt5.QtCore import pyqtSignal, pyqtSlot
-from PyQt5.QtWidgets import QListWidget, QListWidgetItem, QWidget
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import QWidget
+
+from Setting import Setting
 
 from .ui_GameDownloader import Ui_GameDownloader
 
 
 class GameDownloader(QWidget, Ui_GameDownloader):
-    __addItems = pyqtSignal(QListWidget, list)
-
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.setWindowIcon(qta.icon("ph.download-simple"))
-        self.__addItems.connect(lambda a, b: a.addItems(b))
         self.setVersions()
         self.setFabric()
+        self.default_name = ""
 
     @multitasking.task
     def setVersions(self):
-        self.lw_minecraft.clear()
-        self.__addItems.emit(self.lw_minecraft, Version.get_versions())
+        items = Version.get_versions()
+        self.cb_minecraft.clear()
+        self.cb_minecraft.addItems(items)
+        self.set_name()
 
     @multitasking.task
     def setFabric(self):
-        self.lw_fabric.clear()
-        self.lw_fabric.addItems(Version.get_fabric())
+        items = [""] + Version.get_fabric()  # 防止用户在获取版本时点击cb_fabric导致多出一个空项
+        self.cb_fabric.clear()
+        self.cb_fabric.addItems(items)
 
     @multitasking.task
     def setForge(self, version):
-        self.lw_forge.clear()
-        self.lw_forge.addItems(Version.get_forge(version))
+        items = [""] + Version.get_forge(version)  # 同理
+        self.cb_forge.clear()
+        self.cb_forge.addItems(items)
 
-    @pyqtSlot(QListWidgetItem, QListWidgetItem)
-    def on_lw_minecraft_currentItemChanged(self, current, previous):
-        version = current.text()
-        self.setForge(version)
+    @pyqtSlot(str)
+    def on_cb_minecraft_currentTextChanged(self, text):
+        self.setForge(text)
+        self.set_name()
+
+    @pyqtSlot(str)
+    def on_cb_fabric_currentTextChanged(self, text):
+        if text:
+            self.cb_forge.setEnabled(False)
+        else:
+            self.cb_forge.setEnabled(True)
+        self.set_name()
+
+    @pyqtSlot(str)
+    def on_cb_forge_currentTextChanged(self, text):
+        if text:
+            self.cb_fabric.setEnabled(False)
+        else:
+            self.cb_fabric.setEnabled(True)
+        self.set_name()
 
     @pyqtSlot(bool)
     def on_pb_install_clicked(self, _):
         name = self.le_name.text()
-        if self.lw_minecraft.currentItem():
-            version = self.lw_minecraft.currentItem().text()
-            forge_version = fabric_version = ""
-            if self.lw_forge.currentItem():
-                forge_version = self.lw_forge.currentItem().text()
-            if self.lw_fabric.currentItem():
-                fabric_version = self.lw_fabric.currentItem().text()
-            Version(name).install(version, forge_version, fabric_version)
+        Version(name).install(*self.get_versions())
+
+    @pyqtSlot(str)
+    def on_le_name_textChanged(self, name):
+        self.l_warning.setText("")
+        if name == "":
+            return
+        if os.path.exists(
+            os.path.join(Setting().get("game.directories")[0], "versions", name)
+        ):
+            self.l_warning.setText(self.tr("该版本已存在, 继续安装将会覆盖原来内容"))
+
+    def get_versions(self):
+        version = self.cb_minecraft.currentText()
+        forge_version = fabric_version = ""
+        if self.cb_forge.currentText():
+            forge_version = self.cb_forge.currentText()
+        if self.cb_fabric.currentText():
+            fabric_version = self.cb_fabric.currentText()
+        return version, forge_version, fabric_version
+
+    def set_name(self):
+        versions = self.get_versions()
+        if self.le_name.text() == self.default_name:
+            self.default_name = versions[0]
+            if versions[1]:
+                self.default_name += f"-Forge{versions[1].split('-')[-1]}"
+            if versions[2]:
+                self.default_name += f"-Fabric{versions[2]}"
+            self.le_name.setText(self.default_name)
